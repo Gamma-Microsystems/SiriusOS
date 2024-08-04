65e0957de7acf272a7353d995d6c81ede909c5a1
gamma63 <174414990+gamma63@users.noreply.github.com>
2024-08-04 15:04:24 +0300

    base/etc some fixes

--- base/etc/startup.d/50_sirpkg.sh
+++ base/etc/startup.d/50_sirpkg.sh
@@ -1,6 +1,6 @@
 #!/bin/esh
 
-if kcmdline -q no-startup-msk then exit 0
+if kcmdline -q no-startup-sirpkg then exit 0
 
 echo -n "Checking for package updates..." >> /dev/pex/splash
-msk update &
+sirpkg update &
